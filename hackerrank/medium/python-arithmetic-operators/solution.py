2fcefa99f7a4d0b140b63c8806c424efcc763079
Alyaa Selim <alyaaahmed2@gmail.com>
2026-08-07 11:01:21 +0200

    ✅ Arithmetic Operators | Medium | Python | Runtime: N/A | Memory: N/A | 2026-08-07

---
+++ hackerrank/medium/python-arithmetic-operators/solution.py
@@ -0,0 +1,9 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    if a in range(1,10**10) and b in range(1,10**10):
+        print(a+b)
+        print(a-b)
+        print(a*b)
+    else:
+        print("you are not in range ")    
